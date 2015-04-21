c32d0125856442b6ed7287a522ce3701a5dbc32f
MatsDrevstad <mats.drevstad@gmail.com>
2015-04-21 09:54:49 +0200

    grundläggande migrations

---
+++ ErlezWebUI/Migrations/Configuration.cs
@@ -0,0 +1,101 @@
+namespace ErlezWebUI.Migrations
+{
+    using ErlezWebUI.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ErlezWebUI.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(ErlezWebUI.Models.ApplicationDbContext context)
+        {
+            SeedUsers(context);
+        }
+
+        private static void SeedUsers(ErlezWebUI.Models.ApplicationDbContext context)
+        {
+            context.Companies.AddOrUpdate(
+                c => c.CompanyName,
+                new Company { CompanyName = "Edi Solutions" },
+                new Company { CompanyName = "Volvo Personbilar" },
+                new Company { CompanyName = "Volvo Lastvagnar" },
+                new Company { CompanyName = "Ica" },
+                new Company { CompanyName = "Willys" },
+                new Company { CompanyName = "Netto" }
+                );
+
+            if (!context.Roles.Any(r => r.Name == "CanEditUser"))
+            {
+                var roleStore = new RoleStore<IdentityRole>(context);
+                var roleManager = new RoleManager<IdentityRole>(roleStore);
+                roleManager.Create(new IdentityRole { Name = "CanEditUser" });
+            }
+
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+
+            if (!context.Users.Any(u => u.Email == "admin@edisolutions.se"))
+            {
+                var user = new ApplicationUser { CompanyId = 99, Email = "admin@edisolutions.se", UserName = "admin@edisolutions.se" };
+                userManager.Create(user, "qqqq1!Q");
+                userManager.AddToRole(user.Id, "CanEditUser");
+            }
+
+            if (!context.Users.Any(u => u.Email == "helena.robinson@_ica.se"))
+            {
+                var user = new ApplicationUser { RegisterCompanyName = "ICA NÄRA Örebro", Email = "helena.robinson@_ica.se", UserName = "helena.robinson@_ica.se" };
+                userManager.Create(user, "qqqq1!Q");
+            }
+
+            if (!context.Users.Any(u => u.Email == "laura.andrews@_willys.se"))
+            {
+                var user = new ApplicationUser { RegisterCompanyName = "Willy's strand", Email = "laura.andrews@_willys.se", UserName = "laura.andrews@_willys.se" };
+                userManager.Create(user, "qqqq1!Q");
+            }
+
+            if (!context.Users.Any(u => u.Email == "maria.andersson@_ica.se"))
+            {
+                var user = new ApplicationUser { RegisterCompanyName = "ICA kvantum", Email = "maria.andersson@_ica.se", UserName = "maria.andersson@_ica.se" };
+                userManager.Create(user, "qqqq1!Q");
+            }
+
+            if (!context.Users.Any(u => u.Email == "susan.grant@_volvocars.com"))
+            {
+                var user = new ApplicationUser { RegisterCompanyName = "Torslanda", Email = "susan.grant@_volvocars.com", UserName = "susan.grant@_volvocars.com" };
+                userManager.Create(user, "qqqq1!Q");
+            }
+
+            if (!context.Users.Any(u => u.Email == "edgar.stevens@_willys.se"))
+            {
+                var user = new ApplicationUser { RegisterCompanyName = "Willis", Email = "edgar.stevens@_willys.se", UserName = "edgar.stevens@_willys.se" };
+                userManager.Create(user, "qqqq1!Q");
+            }
+
+            if (!context.Users.Any(u => u.Email == "jason.hall@_ica.se"))
+            {
+                var user = new ApplicationUser { RegisterCompanyName = "Fokus (Ica)", Email = "jason.hall@_ica.se", UserName = "jason.hall@_ica.se" };
+                userManager.Create(user, "qqqq1!Q");
+            }
+
+            if (!context.Users.Any(u => u.Email == "bertil.clark@_volvocars.com"))
+            {
+                var user = new ApplicationUser { RegisterCompanyName = "Pinifarina", Email = "bertil.clark@_volvocars.com", UserName = "bertil.clark@_volvocars.com" };
+                userManager.Create(user, "qqqq1!Q");
+            }
+
+            if (!context.Users.Any(u => u.Email == "sandra.lewis@_volvocars.com"))
+            {
+                var user = new ApplicationUser { RegisterCompanyName = "tuve", Email = "sandra.lewis@_volvocars.com", UserName = "sandra.lewis@_volvocars.com" };
+                userManager.Create(user, "qqqq1!Q");
+            }
+        }
+    }
+}
--- ErlezWebUI/Models/IdentityModels.cs
+++ ErlezWebUI/Models/IdentityModels.cs
@@ -3,6 +3,8 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
 
 namespace ErlezWebUI.Models
 {
@@ -16,6 +18,10 @@ namespace ErlezWebUI.Models
             // Add custom user claims here
             return userIdentity;
         }
+
+        public int CompanyId { get; set; }
+        public string RegisterCompanyName { get; set; }
+        public string CompanyName { get; set; }       
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
@@ -29,5 +35,106 @@ namespace ErlezWebUI.Models
         {
             return new ApplicationDbContext();
         }
+
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<CompanyBuyer> CompanyBuyers { get; set; }
+        public DbSet<CompanySeller> CompanySeller { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+
+    public class Company
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+    }
+
+    public class Article
+    {
+        public int Id { get; set; }
+        public string ArticleName { get; set; }
+        public System.Guid Gtin { get; set; }
+        public Nullable<int> CompanySellerId { get; set; }
+        public Nullable<decimal> UnitPrice { get; set; }
+        public string UnitType { get; set; }
+
+        public virtual CompanySeller CompanySeller { get; set; }
+    }
+
+    public class CompanyBuyer
+    {
+        public CompanyBuyer()
+        {
+            this.Orders = new HashSet<Order>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string OrgNo { get; set; }
+        public string City { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+
+    public class CompanySeller
+    {
+        public CompanySeller()
+        {
+            this.Articles = new HashSet<Article>();
+            this.Orders = new HashSet<Order>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string OrgNo { get; set; }
+        public string City { get; set; }
+
+        public virtual ICollection<Article> Articles { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+
+    public class Invoice
+    {
+        public Invoice()
+        {
+            this.Orders = new HashSet<Order>();
+        }
+
+        public int Id { get; set; }
+        public string SellerName { get; set; }
+        public string SellerOrgNo { get; set; }
+        public string SellerCity { get; set; }
+        public string BuyerName { get; set; }
+        public string BuyerOrgNo { get; set; }
+        public string BuyerCity { get; set; }
+        public Nullable<decimal> TotalNet { get; set; }
+        public Nullable<decimal> TotalTax { get; set; }
+        public Nullable<decimal> TotalSum { get; set; }
+        public Nullable<System.DateTime> InvoiceDate { get; set; }
+        public Nullable<System.DateTime> DueDate { get; set; }
+        public Nullable<int> InvoiceNo { get; set; }
+        public string Status { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+        public string OrderType { get; set; }
+        public Nullable<System.DateTime> OrderDate { get; set; }
+        public decimal Amount { get; set; }
+        public string ArticleName { get; set; }
+        public Nullable<System.Guid> Gtin { get; set; }
+        public Nullable<int> InvoiceId { get; set; }
+        public Nullable<int> CompanySellerId { get; set; }
+        public Nullable<int> CompanyBuyerId { get; set; }
+        public Nullable<decimal> UnitPrice { get; set; }
+        public string UnitType { get; set; }
+
+        public virtual CompanyBuyer CompanyBuyer { get; set; }
+        public virtual CompanySeller CompanySeller { get; set; }
+        public virtual Invoice Invoice { get; set; }
     }
 }
